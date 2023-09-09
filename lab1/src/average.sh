c963f2fb04629d08f9bd3986a76db0e06c5f2161
Mark Rabushev <100849266+markrabushev@users.noreply.github.com>
2023-09-09 11:48:51 +0000

    изменения

---
+++ lab1/src/average.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+count=0
+num=0
+while [ -n "$1" ]
+do
+count=$[ $count + 1 ]
+num=$[ $num + $1]
+shift
+done
+avr=$[ $num/$count ]
+echo "Количество = $count"
+echo "Среднее арифметическое = $avr" 
