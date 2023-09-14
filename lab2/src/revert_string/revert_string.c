147db262064e7c031314e1b3f0aa82f02e05c83b
Mark Rabushev <100849266+markrabushev@users.noreply.github.com>
2023-09-14 10:37:03 +0000

    14.9.23 изменения

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,20 @@
 #include "revert_string.h"
 
-void RevertString(char *str)
+void RevertString(char* str)
 {
-	// your code here
+	int len = strlen(str);
+    char *begin, *end;
+    char temp;
+    begin  = str;
+    end    = str;
+    for (int i = 0; i < len - 1; i++)
+        end++;
+ 
+    for (int i = 0; i < len/2; i++) {        
+        temp   = *end;
+        *end   = *begin;
+        *begin = temp;
+        begin++;
+        end--;
+    }
 }
-
